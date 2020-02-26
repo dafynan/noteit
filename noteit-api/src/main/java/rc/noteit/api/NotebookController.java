464088172d2c3ca4ebbc9d4eb1535866139066f5
dafynan <61508268+dafynan@users.noreply.github.com>
2020-02-26 09:45:28 -0500

    Initial Add

---
+++ noteit-api/src/main/java/rc/noteit/api/NotebookController.java
@@ -0,0 +1,55 @@
+package rc.noteit.api;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import rc.noteit.Mapper;
+import rc.noteit.api.viewmodel.NotebookViewModel;
+import rc.noteit.db.NotebookRepository;
+import rc.noteit.model.Notebook;
+
+import javax.validation.ValidationException;
+import java.util.List;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/api/notebooks")
+@CrossOrigin
+public class NotebookController {
+
+    private NotebookRepository notebookRepository;
+
+    private Mapper mapper;
+
+    public NotebookController(NotebookRepository notebookRepository, Mapper mapper) {
+        this.notebookRepository = notebookRepository;
+        this.mapper = mapper;
+    }
+
+    @GetMapping("/all")
+    public List<Notebook> all() {
+        var allCategories = this.notebookRepository.findAll();
+        return allCategories;
+    }
+
+    @PostMapping
+    public Notebook save(@RequestBody NotebookViewModel notebookViewModel,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ValidationException();
+        }
+
+        //From DTO to DAO
+        var notebookEntity = this.mapper.convertToNotebookEntity(notebookViewModel);
+
+        // save notebookEntity (dao) instance to db
+        this.notebookRepository.save(notebookEntity);
+
+        return notebookEntity;
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        this.notebookRepository.deleteById(UUID.fromString(id));
+    }
+}
